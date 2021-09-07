29afe8c9d436c4b0732fb7ea0d5509859baf6f8a
Thomas Nkuna <thomasnkuna96@gmail.com>
2021-09-07 13:07:03 +0200

    using figlet external module

--- greet_xola.js
+++ greet_xola.js
@@ -1,6 +1,10 @@
+var figlet = require('figlet');
 
-const chalk = require('chalk');
-const greet = require('./greet');
-
-const styledMessage = chalk.bgGreen.black(greet('Xola'));
-console.log(styledMessage)
+figlet('Hello, Xola', function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data)
+});
